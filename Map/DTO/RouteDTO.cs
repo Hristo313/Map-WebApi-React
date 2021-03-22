156f507e3ec7e39176d27fb420a5e923a01e37ae
Hristo Hristov <insomnia_2000@abv.bg>
2021-03-22 22:16:04 +0200

    Add attributes

--- Map/DTO/RouteDTO.cs
+++ Map/DTO/RouteDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,10 +10,16 @@ namespace Map.Models
     {
         public int Id { get; set; }
 
+        [Required]
+        [MinLength(3)]
         public string Start { get; set; }
 
+        [Required]
+        [MinLength(3)]
         public string End { get; set; }
 
+        [Required]
+        [MinLength(1)]
         public int Length { get; set; }
     }
 }
--- Map/DTO/TownDTO.cs
+++ Map/DTO/TownDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,8 @@ namespace Map.DTO
 {
     public class TownDTO
     {
+        [Required]
+        [MinLength(3)]
         public string Name { get; set; }
     }
 }
